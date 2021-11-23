039483e8254c7f4b66f852ed141b0097baff4589
Gregory Berngardt <gregoryvit@gmail.com>
2021-11-23 12:01:00 +0300

    Section 2

--- CrewCoreAnimationApp/Scenes/Scene 3/Scene3ViewController.swift
+++ CrewCoreAnimationApp/Scenes/Scene 3/Scene3ViewController.swift
@@ -33,7 +33,57 @@ class Scene3ViewController: UIViewController {
     }
 
     @objc func tapOnScreen(sender: UITapGestureRecognizer) {
-                
+     
+        let touchLocation = sender.location(in: view)
+        
+        let displayLayer = layer.presentation() ?? layer
+        if displayLayer.hitTest(touchLocation) != nil {
+            
+            let resultLayerColor = layer.backgroundColor == UIColor.black.cgColor ? UIColor.red.cgColor : UIColor.black.cgColor
+
+            let bgAnimation = CABasicAnimation(keyPath: "backgroundColor")
+           bgAnimation.fromValue = displayLayer.backgroundColor
+           bgAnimation.toValue = resultLayerColor
+           bgAnimation.duration = 0.5
+            
+            bgAnimation.delegate = self
+
+            layer.backgroundColor = resultLayerColor
+           layer.add(bgAnimation, forKey: "bg")
+            
+        } else {
+            
+            let moveAnimation = CABasicAnimation(keyPath: "position")
+            moveAnimation.fromValue = displayLayer.position
+            moveAnimation.toValue = touchLocation
+            moveAnimation.duration = 3.0
+
+            moveAnimation.delegate = self
+            
+            moveAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+            
+            moveAnimation.setValue("move", forKey: "id")
+                        
+            layer.position = touchLocation
+            layer.add(moveAnimation, forKey: "move")
+            
+        }
+        
     }
         
+}
+
+
+extension Scene3ViewController: CAAnimationDelegate {
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        
+        guard let id = anim.value(forKey: "id") as? String else { return }
+        
+        switch id {
+        case "move": print("Animation done!")
+        default: break
+        }
+    }
+    
 }
